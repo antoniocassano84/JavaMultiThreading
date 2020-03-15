2d282d0a4746ae28f265b8601bd9a31b2b3c0bc2
antoniocassano84 <antonio.cassano984@gmail.com>
2020-03-15 06:59:24 +0000

    using lambda

--- src/UseThreads2.java
+++ src/UseThreads2.java
@@ -1,50 +1,27 @@
-// Create a thread by implementing runnable.
-
-class MyThread2 implements Runnable {
-    String thrdName;
-
-    MyThread2(String name) {
-        thrdName = name;
-    }
-
-    // Entry point of thread.
-    public void run() {
-        System.out.println(thrdName + " starting.");
-        try {
-            for(int count =0; count < 10; count++) {
-                Thread.sleep(400);
-                System.out.println("In " + thrdName + ", count is " + count);
-            }
-        } catch (InterruptedException exc) {
-            System.out.println(thrdName + " interrupted.");
-        }
-        System.out.println(thrdName + " terminating.");
-    }
-}
-
 class UseThreads2 {
 
     public static void main(String[] args) {
-        System.out.println("Main thread starting.");
+        String mainT = Thread.currentThread().getName();
+        System.out.println(mainT + " thread starting.");
 
-        // First, construct a MyThread object.
-        MyThread2 mt = new MyThread2("Child #1");
-
-        // Next, construct a thread from that object.
-        Thread newThrd = new Thread(mt);
-
-        // Finally, start execution of the thread.
-        newThrd.start();
+        (new Thread( () -> { // This is the new Thread
+            String thrdName = "Child #1";
+            System.out.println("\t" + thrdName + " starting.");
+            try {
+                for(int i =0; i<10; i++) {
+                    Thread.sleep(400);
+                    System.out.println("\t" + thrdName + ": count is " + i);
+                }
+            } catch (InterruptedException exc) { System.out.println(thrdName + " interrupted."); }
+            System.out.println(thrdName + " terminating.");
+        } // End Child Thread
+        )).start();
 
         for(int i=0; i<50; i++) {
-            System.out.print(".");
-            try {
-                Thread.sleep(100);
-            } catch(InterruptedException exc) {
-                System.out.println("Main thread interrupted");
-            }
+            System.out.println(mainT + " thread: pass #" + i);
+            try { Thread.sleep(100); }
+            catch(InterruptedException exc) { System.out.println(mainT + " thread interrupted"); }
         }
-
-        System.out.println("Main thread ending.");
+        System.out.println(mainT + " thread ending.");
     }
 }
